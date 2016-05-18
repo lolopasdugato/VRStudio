b5f5f3a2be12ce376dd83e72275451e24f8340c0
Gabriel <gabriel.rassoul@gmail.com>
2016-05-18 15:09:55 -0400

    test collisions

---
+++ Assets/Tap.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tap : MonoBehaviour
+{
+
+    Material mat;
+
+    // Use this for initialization
+    void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mat.color = Color.green;
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name.Contains("bone"))
+            mat.color = Color.blue;
+    }
+
+    void OnTriggerStay(Collider col)
+    {
+        if (col.gameObject.name.Contains("bone"))
+            mat.color = Color.red;
+    }
+}
