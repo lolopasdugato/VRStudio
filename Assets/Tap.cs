d245dc5ed73d44ff48162c3147bc9d19a6617bc2
Gabriel <gabriel.rassoul@gmail.com>
2016-05-20 09:03:56 -0400

    tap

--- Assets/Tap.cs
+++ Assets/Tap.cs
@@ -4,29 +4,17 @@ using System.Collections;
 public class Tap : MonoBehaviour
 {
 
-    Material mat;
+    AudioSource src;
 
     // Use this for initialization
     void Start()
     {
-        mat = GetComponent<Renderer>().material;
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        mat.color = Color.green;
+        src = GetComponent<AudioSource>();
     }
 
     void OnTriggerEnter(Collider col)
     {
         if (col.gameObject.name.Contains("bone"))
-            mat.color = Color.blue;
-    }
-
-    void OnTriggerStay(Collider col)
-    {
-        if (col.gameObject.name.Contains("bone"))
-            mat.color = Color.red;
+            src.Play();
     }
 }
