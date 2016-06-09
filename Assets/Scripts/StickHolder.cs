72319bb9060a32caced1cbab00448128f20915a3
Gnufgnuf <etienvia@gmail.com>
2016-06-09 14:51:52 -0400

    add manquant

---
+++ Assets/Scripts/StickHolder.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+using Leap.Unity;
+
+public class StickHolder : MonoBehaviour {
+
+	Hand hand;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (gameObject.GetComponent<RigidHand> ().GetLeapHand ().GrabStrength < 0.2) {
+			transform.Find ("palm/stick").gameObject.SetActive (false);
+		} else {
+			transform.Find ("palm/stick").gameObject.SetActive (true);
+		}
+	}
+		
+}
---
+++ Assets/Scripts/Tap.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Tap : MonoBehaviour
+{
+
+    AudioSource src;
+
+    // Use this for initialization
+    void Start()
+    {
+        src = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.name.Contains("stick"))
+            src.Play();
+    }
+}
